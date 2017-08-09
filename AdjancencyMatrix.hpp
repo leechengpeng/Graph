2071709e1e5958babb68796360824c5d98d73b3e
leechengpeng <leechengpeng@126.com>
2017-08-09 09:21:27 +0800

    Create AdjancencyMatrix.hpp

---
+++ AdjancencyMatrix.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include <vector>
+
+namespace Graph
+{
+	template <typename T, unsigned N>
+	class AdjancencyMatrix
+	{
+	public:
+		AdjancencyMatrix() : m_Matrix() {}
+
+		void addEdge(size_t vSourceIndex, size_t vDestIndex, T vWeight = T());
+		bool connect(size_t vSourceIndex, size_t vDestIndex) const;
+		T    getEdgeWeight(size_t vSourceIndex, size_t vDestIndex) const;
+
+		// TODO：Using iterator to visit inner element
+		std::vector<size_t> getAdjancencyVertices(size_t vVertexIndex) const;
+
+	private:
+		T m_Matrix[N][N];
+	};
+
+	template <typename T, unsigned N>
+	void AdjancencyMatrix<T, N>::addEdge(size_t vSourceIndex, size_t vDestIndex, T vWeight)
+	{
+		_ASSERT(vSourceIndex < N && vDestIndex < N);
+		// NOTE：Vertex X to X`s self default zero
+		if (vSourceIndex != vDestIndex)
+		{
+			m_Matrix[vSourceIndex][vDestIndex] = vWeight;
+			m_Matrix[vDestIndex][vSourceIndex] = vWeight;
+		}
+	}
+
+	template <typename T, unsigned N>
+	bool AdjancencyMatrix<T, N>::connect(size_t vSourceIndex, size_t vDestIndex) const
+	{
+		_ASSERT(vSourceIndex < N && vDestIndex < N);
+		return m_Matrix[vSourceIndex][vDestIndex] != 0;
+	}
+
+	template <typename T, unsigned N>
+	T AdjancencyMatrix<T, N>::getEdgeWeight(size_t vSourceIndex, size_t vDestIndex) const
+	{
+		_ASSERT(vSourceIndex < N && vDestIndex < N);
+		return m_Matrix[vSourceIndex][vDestIndex];
+	}
+
+	template <typename T, unsigned N>
+	std::vector<size_t> AdjancencyMatrix<T, N>::getAdjancencyVertices(size_t vVertexIndex) const
+	{
+		_ASSERT(vVertexIndex < N);
+		
+		std::vector<size_t> AdjancencyVertices;
+		for (size_t i = 0; i < N; ++i)
+		{
+			if (m_Matrix[vVertexIndex][i] != 0)
+			{
+				AdjancencyVertices.push_back(i);
+			}
+		}
+
+		return AdjancencyVertices;
+	}
+}
