d00609183ae5b255d8840bf7d0931d339d2f721b
leechengpeng <leechengpeng@126.com>
2017-08-10 21:40:04 +0800

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -6,6 +6,31 @@
 namespace Graph
 {
 	template <typename T, unsigned N>
+	T dijkstra(const AdjancencyMatrix<T, N>& vAdjancencyMatrix, size_t vSource, size_t vDest, std::vector<size_t>& vPath)
+	{
+		_ASSERT(vSource < N && vDest < N && vPath.empty());
+
+		T      Dest[N] = { 0 };
+		size_t PrevVertices[N] = { 0 };
+
+		Graph::dijkstra(vAdjancencyMatrix, vSource, PrevVertices, Dest);
+
+		std::vector<size_t> TempPath;
+		for (size_t i = vDest; i != vSource; i = PrevVertices[i])
+		{
+			TempPath.push_back(i);
+		}
+		TempPath.push_back(vSource);
+
+		for (auto Iter = TempPath.rbegin(); Iter != TempPath.rend(); ++Iter)
+		{
+			vPath.push_back(*Iter);
+		}
+
+		return Dest[vDest];
+	}
+
+	template <typename T, unsigned N>
 	void dijkstra(const AdjancencyMatrix<T, N>& vAdjancencyMatrix, size_t vSource, size_t vPrevVertex[], T vDest[])
 	{
 		_ASSERT(vSource < N);
@@ -46,49 +71,42 @@ namespace Graph
 	}
 
 	template <typename T, unsigned N>
-	T dijkstra(const AdjancencyMatrix<T, N>& vAdjancencyMatrix, size_t vSource, size_t vDest, std::vector<size_t>& vPath)
+	T prim(const AdjancencyMatrix<T, N>& vAdjancencyMatrix, AdjancencyMatrix<T, N>& voMST)
 	{
-		_ASSERT(vSource < N && vDest < N && vPath.empty());
-
-		T      Dest[N] = { 0 };
-		size_t PrevVertices[N] = { 0 };
-
-		Graph::dijkstra(vAdjancencyMatrix, vSource, PrevVertices, Dest);
-
-		std::vector<size_t> TempPath;
-		for (size_t i = vDest; i != vSource; i = PrevVertices[i])
-		{
-			TempPath.push_back(i);
-		}
-		TempPath.push_back(vSource);
-
-		for (auto Iter = TempPath.rbegin(); Iter != TempPath.rend(); ++Iter)
-		{
-			vPath.push_back(*Iter);
-		}
-
-		return Dest[vDest];
-	}
-
-	template <typename T, unsigned N>
-	T prim(const AdjancencyMatrix<T, N>& vAdjancencyMatrix, std::vector<size_t>& vMST)
-	{
-		_ASSERT(vMST);
 		static T INF = _TYPE_MAX<T>();
 
+		T	   Dest[N]	     = { INF };
+		size_t PrevVertex[N] = { 0 };
 		size_t Source = 0;
 
-		T	 Dest[N]	= { INF };
-		bool Visited[N] = { false };
+		T MSTWeight = prim(vAdjancencyMatrix, Source, PrevVertex, Dest);
 
 		for (size_t i = 0; i < N; ++i)
 		{
-			if (vAdjancencyMatrix.connect(Source, i))
+			if (i != Source)
 			{
-				Dest[i] = vAdjancencyMatrix.getEdgeWeight(Source, i);
+				voMST.addEdge(i, PrevVertex[i], Dest[i]);
 			}
 		}
-		Visited[Source] = true;
+
+		return MSTWeight;
+	}
+
+	template <typename T, unsigned N>
+	T prim(const AdjancencyMatrix<T, N>& vAdjancencyMatrix, size_t vSource, size_t vPrevVertex[], T vDest[])
+	{
+		_ASSERT(vSource < N);
+		static T INF = _TYPE_MAX<T>();
+
+		bool Visited[N] = { false };
+		for (size_t i = 0; i < N; ++i)
+		{
+			if (vAdjancencyMatrix.connect(vSource, i))
+			{
+				vDest[i] = vAdjancencyMatrix.getEdgeWeight(vSource, i);
+			}
+		}
+		Visited[vSource] = true;
 
 		T MSTWeight = T();
 		for (size_t i = 1; i < N; ++i)
@@ -97,23 +115,23 @@ namespace Graph
 			size_t MinIndex = 0;
 			for (size_t k = 0; k < N; ++k)
 			{
-				if (!Visited[k] && Dest[k] <= MinEdge)
+				if (!Visited[k] && vDest[k] <= MinEdge)
 				{
 					MinIndex = k;
-					MinEdge  = Dest[k];
+					MinEdge  = vDest[k];
 				}
 			}
 			Visited[MinIndex] = true;
 
-			vMST.push_back(MinIndex);
 			MSTWeight += MinEdge;
 
 			for (size_t k = 0; k < N; ++k)
 			{
 				if (!Visited[k] && vAdjancencyMatrix.connect(MinIndex, k) 
-					&& vAdjancencyMatrix.getEdgeWeight(MinIndex, k) < Dest[k])
+					&& vAdjancencyMatrix.getEdgeWeight(MinIndex, k) < vDest[k])
 				{
-					Dest[k] = vAdjancencyMatrix.getEdgeWeight(MinIndex, k);
+					vDest[k] = vAdjancencyMatrix.getEdgeWeight(MinIndex, k);
+					vPrevVertex[k] = MinIndex;
 				}
 			}
 		}
